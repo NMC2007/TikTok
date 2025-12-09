9185dbc8cf98feb8d7415531a221a81a11bc1e8b
NMC2007 <nmc281207@gmail.com>
2025-12-09 14:44:22 +0700

    update version react and react-dom, update ui tippy preview account

--- src/components/Popper/PreviewAccounts/PreviewAccounts.js
+++ src/components/Popper/PreviewAccounts/PreviewAccounts.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import style from './PreviewAccounts.module.scss';
+import Button from '~/components/Button';
 
 const cx = classNames.bind(style);
 
@@ -15,9 +18,26 @@ function PreviewAccounts({ children }) {
             offset={[12, 8]}
             placement="bottom-start"
             render={(attrs) => (
-                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                <div className={cx('wrapper')} tabIndex="-1" {...attrs}>
                     <PopperWrapper className={cx('menu-popper')}>
-                        <div className={cx('menu-body')}>Hello</div>
+                        <header className={cx('header')}>
+                            <img className={cx('avatar')} src="" alt="" />
+                            <Button primary>Folowing</Button>
+                        </header>
+                        <div className={cx('body')}>
+                            <p className={cx('nickname')}>
+                                <strong>NguyenManhCuong</strong>
+                                <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                            </p>
+                            <p className={cx('name')}>Nguyen Manh Cuong</p>
+
+                            <p>
+                                <strong>1M</strong>
+                                <span>Folowing</span>
+                                <strong>1M</strong>
+                                <span>Likes</span>
+                            </p>
+                        </div>
                     </PopperWrapper>
                 </div>
             )}
